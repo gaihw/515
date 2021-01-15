783bb8a4455620b1a037bc1ae9c407b445cb71a8
root <13020071928@163.com>
2021-01-15 19:46:02 +0800

    提交testng项目

---
+++ comautor/src/main/java/com/test/cases/demo.java
@@ -0,0 +1,16 @@
+package com.test.cases;
+
+import org.testng.annotations.Test;
+
+public class demo {
+
+    @Test
+    public void one(){
+        System.out.println("one ...");
+    }
+
+    @Test
+    public void two(){
+        System.out.println("two ...");
+    }
+}
