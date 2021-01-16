3baf6aa8b60fa93a5ffcdbbc9768f4c4e74f5d1f
root <13020071928@163.com>
2021-01-16 15:52:22 +0800

    用例3失败

--- comautor/src/main/java/com/test/cases/demo.java
+++ comautor/src/main/java/com/test/cases/demo.java
@@ -1,8 +1,12 @@
 package com.test.cases;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class demo {
+    public static void main(String[] args) {
+
+    }
 
     @Test
     public void one(){
@@ -12,5 +16,10 @@ public class demo {
     @Test
     public void two(){
         System.out.println("two ...");
+    }
+
+    @Test
+    public void three(){
+        Assert.assertEquals(1,2);
     }
 }
