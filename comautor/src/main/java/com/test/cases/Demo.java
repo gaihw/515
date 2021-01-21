d3d3dfa51f2b4cbb00a37b84aaa7b3c1880687a7
root <13020071928@163.com>
2021-01-21 15:14:18 +0800

    修改testng.xml内容

---
+++ comautor/src/main/java/com/test/cases/Demo.java
@@ -0,0 +1,38 @@
+package com.test.cases;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Demo {
+    static {
+        System.setProperty("fileName", "contract/info.log");
+    }
+    public static Logger log = LoggerFactory.getLogger(Demo.class);
+
+    @BeforeClass
+    public void b(){
+        System.out.println("b ...");
+        log.info("b ....");
+    }
+    @Test
+    public void one(){
+        System.out.println("one ...");
+        log.info("one ....");
+    }
+    @Test
+    public void three(){
+        Assert.assertEquals(1,1);
+    }
+
+
+
+    @Test
+    public void two(){
+        System.out.println("two ...");
+    }
+
+
+}
