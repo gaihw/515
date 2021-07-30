df0fd85c7afe08505210d5689494396c5d7f89b8
hongwei.gai <hongwei.gai@allintechinc.com>
2021-07-30 20:48:54 +0800

    mongo测试库连接

--- gai01/src/main/java/allin/prometheus/Demo.java
+++ gai01/src/main/java/allin/prometheus/Demo.java
@@ -14,6 +14,7 @@ import org.slf4j.LoggerFactory;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 public class Demo {
@@ -57,6 +58,7 @@ public class Demo {
         }
 
 
-
+        Calendar cal = Calendar.getInstance();
+        cal.get(Calendar.DATE);
     }
 }
